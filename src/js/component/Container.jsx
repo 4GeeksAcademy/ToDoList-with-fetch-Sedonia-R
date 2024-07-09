8f55af4d3435c09cbbb53ed3437c8010479d853e
Sedonia Raethstein <166647392+Sedonia-R@users.noreply.github.com>
2024-07-09 22:35:37 +0000

    added button to clear list, but just styling

--- src/js/component/Container.jsx
+++ src/js/component/Container.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { TopInput } from './TopInput';
 import { Tasks } from './Tasks';
 import { Footer } from './Footer';
+import { DeleteButton } from './DeleteButton';
 
 export const Container = () => {
     const [ inputValue, setInputValue ] = useState('');
@@ -15,12 +16,13 @@ export const Container = () => {
     }
 
     return (
-        <div className="row gx-0">
+        <div className="row gx-0 to-do">
             <div className='paper-stack'>
                 <TopInput inputValue={inputValue} setInputValue={setInputValue} handleInputEnter={handleInputEnter}/>
                 <Tasks allTasks={allTasks} setAllTasks={setAllTasks}/>
                 <Footer allTasks={allTasks}/>
             </div>
+            <DeleteButton/>
         </div>
     );
 };
---
+++ src/js/component/DeleteButton.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const DeleteButton = () => {
+    return (
+        <button className='btn delete'>Clear All Tasks</button>
+    );
+};
